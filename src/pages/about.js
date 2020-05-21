4f76ca0bd73bc5d45d83102ee3035cbf1ed38d41
gortron <gordon.lanza@gmail.com>
2020-05-20 17:30:00 -0700

    added about page

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+export default () => (
+  <>
+    <h1>This is About</h1>
+    <p>this is a about page</p>
+    <Link to="/"> &larr; go home</Link>
+  </>
+);
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,10 @@
 import React from 'react';
+import { Link } from 'gatsby';
 
-export default () => <div>Hello Frontend Masters!</div>;
+export default () => (
+  <>
+    <h1>This is Home</h1>
+    <p>this is a home page</p>
+    <Link to="/about">go to about &rarr;</Link>
+  </>
+);
